0d00571fe80e3e6bd602ad180890cd8d6f402999
Gregory Jefferis <jefferis@gmail.com>
2008-08-31 09:40:17 +0100

    Added a simplified version of plotneuron3d with faster/more stable rgl calls
    - fixes colour problem (by offloading that issue straight to rgl)
    - faster drawing by adding all line segments at once

---
+++ R/Code/plotneuron3d.R
@@ -0,0 +1,37 @@
+plotneuron3d.simple<-function(ANeuron, WithLine=T,
+	WithNodes=T,WithAllPoints=F,WithText=F,PlotSubTrees=T,ClearRGL=T,NeuronList=MyNeurons,...){
+	# rewrite of plotneuron3d using updated rgl calls
+	
+	require(rgl)
+	
+    if (is.character(ANeuron)){
+	ANeuron<-NeuronList[[GetNeuronNum(ANeuron)]]
+    }
+    if (is.numeric(ANeuron)){
+	ANeuron<-NeuronList[[ANeuron]]
+    }
+    
+    if (!is.list(ANeuron)){
+	warning("Cannot understand passed neuron")
+	return(F)
+    }
+	
+	NodesOnly<-c(ANeuron$BranchPoints,
+	    ANeuron$EndPoints[-which(ANeuron$EndPoints==ANeuron$StartPoint)],
+	    ANeuron$StartPoint)
+	NodeCols<-c(rep("red",length(ANeuron$BranchPoints)),
+	    rep("green",length(ANeuron$EndPoints)-1),"purple" )
+	
+	if(WithNodes){
+		if(!WithLine) NodeCols=rep(Colour,length(NodeCols))
+		points3d(ANeuron$d[NodesOnly,c("X","Y","Z")],color=NodeCols,size=3)
+		if(WithText) # text labels for nodes
+		rgl.texts(ANeuron$d[NodesOnly,c("X","Y","Z")],NodesOnly,color=NodeCols,justify="right")		
+	}
+	
+	d=data.matrix(ANeuron$d[,c("X","Y","Z")])
+	# xyzl=sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)})
+	# NAs are used to break line segments
+	xyzl<-do.call(rbind,sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)}))
+	lines3d(xyzl,...)
+}
