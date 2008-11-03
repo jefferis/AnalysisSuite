f1c5abc28090dbc60e03de15a96495ac925c1087
Gregory Jefferis <jefferis@gmail.com>
2008-11-03 16:08:19 +0000

    further improvements to plotneuron3d.simple (colours, highlight long seg)
    - for multiple neurons, can now pass a colour function
    - can also pass a factor in which case neurons will be rainbow coloured
      by group
    - can highlight longest segment of neuron
    - wrapped sapply in invisible so don't have to look at list of return values.

--- R/Code/plotneuron3d.R
+++ R/Code/plotneuron3d.R
@@ -1,14 +1,21 @@
 plotneuron3d.simple<-function(ANeuron, WithLine=T,
-	WithNodes=T,WithAllPoints=F,WithText=F,PlotSubTrees=T,ClearRGL=T,NeuronList=MyNeurons,...){
+	WithNodes=T,WithAllPoints=F,WithText=F,HighlightLongestSegment=FALSE,PlotSubTrees=T,ClearRGL=T,NeuronList=MyNeurons,col,...){
 	# rewrite of plotneuron3d using updated rgl calls
 	
 	require(rgl)
 	if(ClearRGL) rgl.clear()
     
+	if(missing(col)) col='green'
+
 	if ( is.character(ANeuron) || is.numeric(ANeuron) ){
-		if(length(ANeuron)>1) return(sapply(NeuronList[ANeuron],
-			plotneuron3d.simple, WithNodes=WithNodes,WithText=WithText,WithLine=WithLine,ClearRGL=FALSE,...))
-		else ANeuron<-NeuronList[[ANeuron]]
+		if(length(ANeuron)>1){
+			if(is.function(col)) col=col(length(ANeuron))
+			if(is.factor(col)) col=rainbow(nlevels(col))[as.integer(col)]
+			return(invisible(mapply(plotneuron3d.simple,
+			NeuronList[ANeuron],col=col,ClearRGL=FALSE,
+			WithNodes=WithNodes,WithText=WithText,WithLine=WithLine,
+			HighlightLongestSegment=HighlightLongestSegment)))
+		} else ANeuron<-NeuronList[[ANeuron]]
     }
     
     if (!is.list(ANeuron)){
@@ -23,6 +30,7 @@ plotneuron3d.simple<-function(ANeuron, WithLine=T,
 	    rep("green",length(ANeuron$EndPoints)-1),"purple" )
 	
 	if(WithNodes){
+		Colour=col
 		if(!WithLine) NodeCols=rep(Colour,length(NodeCols))
 		points3d(ANeuron$d[NodesOnly,c("X","Y","Z")],color=NodeCols,size=3)
 		if(WithText) # text labels for nodes
@@ -33,5 +41,9 @@ plotneuron3d.simple<-function(ANeuron, WithLine=T,
 	# xyzl=sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)})
 	# NAs are used to break line segments
 	xyzl<-do.call(rbind,sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)},simplify=FALSE))
-	lines3d(xyzl,...)
+	lines3d(xyzl,col=col,...)
+	if(HighlightLongestSegment){
+		x=GetLongestSegment(ANeuron)
+		spheres3d(x,col=col,...)
+	}
 }
