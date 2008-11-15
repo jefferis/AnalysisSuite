2d41e06a84305161791a83c3d309bd917d958785
Gregory Jefferis <jefferis@gmail.com>
2008-11-15 21:10:52 +0000

    Fix a bug in EuclideanLinesetDistance (and add more comments)

--- R/Code/LineMatching.R
+++ R/Code/LineMatching.R
@@ -6,8 +6,17 @@
 
 # After Kamgar-Parsi and Kamgar-Parsi, 2004
 # http://ieeexplore.ieee.org/iel5/34/28505/01273930.pdf?arnumber=1273930
+
 LinesetEuclideanDistance<-function(l1, l2){
 	# l1 and l2 are sets of l+1 points each defining l segments
+
+	# For matching, there should be n equal length segments
+	# Reference: An = (an,bhatn,Ln)
+	# Sample:    Xn = (xn,yhatn,Ln)
+	# point, unit direction vector, Length
+	
+	# distance = Sum for n=1 to N
+	# [ln ||an-xn||^2 +ln^3(1-bhatn.yhatn)/6]
 	
 	l1=data.matrix(l1)
 	l2=data.matrix(l2)
@@ -16,7 +25,7 @@ LinesetEuclideanDistance<-function(l1, l2){
 	l1.diffs=diff(l1)
 	l2.diffs=diff(l2)
 	
-	# Calculate squared segment lengths
+	# Calculate segment lengths
 	l1.seglengths=normbyrow(l1.diffs)
 	l2.seglengths=normbyrow(l2.diffs)
 		
@@ -30,7 +39,7 @@ LinesetEuclideanDistance<-function(l1, l2){
 	
 	# Calculate the point displacement term
 	pointDiffs=l1[-nrow(l1),]-l2[-nrow(l2),]
-	m1=l1.seglengths*normbyrow(pointDiffs)
+	m1=l1.seglengths*normbyrow(pointDiffs)^2
 
 	# Calculate the line angle mismatch term	
 	m2=l1.seglengths^3/6*(1-dotprod(l1.diffs,l2.diffs))
