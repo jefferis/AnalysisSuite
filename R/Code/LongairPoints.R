96baeb219bcf0fbd86736cdff851c6ab65547e9a
Gregory Jefferis <jefferis@gmail.com>
2009-01-20 04:47:15 +0000

    Added a function to read Mark Longair's tracing format
    - this is as generated by his Simple Neurite Tracer plugin
      which generates a gzipped xml file
    - doesn't yet parse fitted neurites
    - still needs tests

--- R/Code/LongairPoints.R
+++ R/Code/LongairPoints.R
@@ -11,3 +11,43 @@ ReadLongairPoints<-function(f){
 	close(tc)
 	points
 }
+
+require(XML)
+ReadLongairTraceData<-function(f,Verbose=TRUE){
+	doc<-xmlTreeParse(f)
+	r<-xmlRoot(doc)
+	if(xmlName(r)!="tracings") stop("This is not a Longair format tracing")
+	
+	l<-list()
+	stopifnot(names(r)[1:2]==c("samplespacing","imagesize"))	
+	attr(l,"samplespacing")<-r['samplespacing'][[1]]
+	attr(l,"imagesize")<-r['imagesize'][[1]]
+	
+	tracings=r[-c(1:2)]
+	if(length(tracings)==0) stop("No tracings in this file")
+	if(Verbose) cat("There are",length(tracings),"tracings in this file\n")	
+	
+	for(i in 1:length(tracings)){
+		l[[i]]=xmlSApply(tracings[[i]],function(x) as.numeric(xmlAttrs(x)[c("xd","yd","zd")]))
+		l[[i]]=t(l[[i]])
+		rownames(l[[i]])<-NULL
+		colnames(l[[i]])<-c("X","Y","Z")
+		attr(l[[i]],'pathAttributes')=xmlAttrs(tracings[[i]])
+	}
+	l
+}
+
+ReadNeuronsFromLongairTraces<-function(f,...){
+	l=ReadLongairTraceData(f,...)
+	neuronList=list()
+	for(i in seq(l)){
+		d=l[[i]]
+		df=data.frame(PointNo=1:nrow(d),Label=2)
+		df=cbind(df,d)
+		df$radius=1
+		df$Parent=df[,1]-1
+		df$Parent[0]=-1
+		neuronList[[i]]=ParseSWCTree(df,f)
+	}
+	neuronList
+}
