a1ac90df94341cbb5ffc7c6127b4e9eba8d51cfe
Gregory Jefferis <jefferis@gmail.com>
2009-02-21 19:08:00 +0000

    Fix reading of Longair Tracings with higher order branching

--- R/Code/LongairPoints.R
+++ R/Code/LongairPoints.R
@@ -40,6 +40,9 @@ ReadLongairTraceData<-function(f,Verbose=TRUE){
 ReadNeuronsFromLongairTraces<-function(f,...){
 	l=ReadLongairTraceData(f,...)
 	dflist=list()
+	pointOffsets=rep(0,length(l))
+	MasterPath=seq(l)
+	# numPoints=rep(0,length(l))
 	for(i in seq(l)){
 		d=l[[i]]
 		df=data.frame(PointNo=1:nrow(d),Label=2)
@@ -47,20 +50,34 @@ ReadNeuronsFromLongairTraces<-function(f,...){
 		df$radius=1
 		df$Parent=df[,1]-1
 		pathAttributes=attr(l[[i]],"pathAttributes")
-		
+		# numPoints[i]=nrow(df)
 		if('startson'%in%names(pathAttributes)){
 			# this path is actually joined to another
 			# nb Mark's paths are 0 indexed (R is 1 indexed)
+			
+			# find out which path this is joined to
+			parentPathId=as.numeric(pathAttributes['startson'])+1
+			# now find the Master Path of that path
+			MasterPath[i]=MasterPath[parentPathId]
+			
+			# now find the index of the point in the parent path to which this path is corrected
 			parentStartIndex=as.numeric(pathAttributes['startsindex'])+1
-			# set the parent of this new path to the point on parent path
+			# ... and correct this (in case that path was not path 0)
+			parentStartIndex=parentStartIndex+pointOffsets[parentPathId]
+			
+			# now set the parent of this new path to the point on parent path
 			df$Parent[1]=parentStartIndex
+						
+			# make a note of the number of points by which we have to offset
+			# points for this path
+			pointOffsets[i]=nrow(dflist[[MasterPath[i]]])
+			
 			# adjust all other point ids to start by the number of rows
 			# in the parent path data frame
-			parentPathId=as.numeric(pathAttributes['startson'])+1			
-			df$Parent[-1]=df$Parent[-1]+nrow(dflist[[parentPathId]])
-			df$PointNo=df$PointNo+nrow(dflist[[parentPathId]])
-			# now add these data to the dataframe for the parent path
-			dflist[[parentPathId]]=rbind(dflist[[parentPathId]],df)
+			df$Parent[-1]=df$Parent[-1]+pointOffsets[i]
+			df$PointNo=df$PointNo+pointOffsets[i]
+			# now add these data to the dataframe for the master path
+			dflist[[MasterPath[i]]]=rbind(dflist[[MasterPath[i]]],df)
 			dflist[[i]]=NA
 		} else {
 			df$Parent[1]=-1
