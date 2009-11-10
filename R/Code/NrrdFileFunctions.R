ae01959cebf15cf985e45c47ec0d1fafab801fa8
Gregory Jefferis <jefferis@gmail.com>
2009-11-10 21:21:39 +0000

    Add Nrrd2op that can be used eg for merging masks

--- R/Code/NrrdFileFunctions.R
+++ R/Code/NrrdFileFunctions.R
@@ -230,6 +230,22 @@ ReadHistogramFromNrrd<-function(filename,...){
 	"density", "mids", "xname", "equidist"), class = "histogram")
 }
 
+Nrrd2op<-function(infiles,outfile,unu2opfun=c("max","min","+", "-", "x", "/"),CreateDirs=TRUE,Verbose=TRUE,Force=FALSE){
+	if(length(infiles)<1) return(NULL)
+	# Do nothing if inputs are older than output unless Force=T
+	if(!Force && !RunCmdForNewerInput(NULL,infiles,outfile)) return (NULL)
+	if(CreateDirs && !file.exists(dirname(outfile))) dir.create(dirname(outfile))
+	file.copy(infiles[1],outfile)
+	if(length(infiles)==1) return(1)
+	unu2opfun=match.arg(unu2opfun)
+	for (f1 in infiles[-1]){
+		cmd=paste("unu 2op",unu2opfun,
+			shQuote(f1),shQuote(outfile),"-o",shQuote(outfile))
+		system(cmd)
+		if(Verbose) cat(".")
+	}
+}
+
 NrrdMinMax<-function(filename,...){
 	minmax=.callunu("minmax",shQuote(filename),...)
 	minmax=sub("^.*")
