457435cfd0ee8bfd256e9830378c7e7aab732270
Gregory Jefferis <jefferis@gmail.com>
2009-11-13 19:20:05 +0000

    Make Nrrd2op a little more efficient for just 2 files.

--- R/Code/UNUImageProcessingFunctions.R
+++ R/Code/UNUImageProcessingFunctions.R
@@ -7,15 +7,27 @@ Nrrd2op<-function(infiles,outfile,unu2opfun=c("max","min","+", "-", "x", "/"),Cr
 	# Do nothing if inputs are older than output unless Force=T
 	if(!Force && !RunCmdForNewerInput(NULL,infiles,outfile)) return (NULL)
 	if(CreateDirs && !file.exists(dirname(outfile))) dir.create(dirname(outfile))
-	file.copy(infiles[1],outfile)
-	if(length(infiles)==1) return(1)
 	unu2opfun=match.arg(unu2opfun)
-	for (f1 in infiles[-1]){
+	
+	# 1 input: f1 -> output
+	if(length(infiles)==1) file.copy(infiles[1],outfile)
+	else if(length(infiles)>1) {
+		# 2 or more inputs: f1 x f2 -> output
 		cmd=paste("unu 2op",unu2opfun,
-			shQuote(f1),shQuote(outfile),"-o",shQuote(outfile))
+			shQuote(infiles[1]),shQuote(infiles[2]),"-o",shQuote(outfile))
 		system(cmd)
 		if(Verbose) cat(".")
 	}
+	# 3 or more inputs: f3 ... x output -> output
+	if(length(infiles)>2) {
+		for (f in infiles[-(1:2)]){
+			cmd=paste("unu 2op",unu2opfun,
+				shQuote(f),shQuote(outfile),"-o",shQuote(outfile))
+			system(cmd)
+			if(Verbose) cat(".")
+		}		
+	}
+	return(length(infiles))
 }
 
 NrrdMinMax<-function(filename,...){
