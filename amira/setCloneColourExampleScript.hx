bb9ecbb17aeb3e31c756e244f977ad9c1c925427
Gregory Jefferis <jefferis@gmail.com>
2009-11-14 14:41:47 +0000

    Add example setCloneColour script

---
+++ amira/setCloneColourExampleScript.hx
@@ -0,0 +1,20 @@
+# Amira Script
+ShowClones.scro setCloneColour "AL-d_F" 1 0 0
+ShowClones.scro setCloneColour "AL-d_M" 1 0.317647058823529 0
+ShowClones.scro setCloneColour "AL-j_F" 1 0.631372549019608 0
+ShowClones.scro setCloneColour "AL-j_M" 1 0.949019607843137 0
+ShowClones.scro setCloneColour "AL-PNs" 0.737254901960784 1 0
+ShowClones.scro setCloneColour "aSP2-b_F" 0.419607843137255 1 0
+ShowClones.scro setCloneColour "aSP2-b_M" 0.105882352941176 1 0
+ShowClones.scro setCloneColour "aSP2-c_F" 0 1 0.211764705882353
+ShowClones.scro setCloneColour "aSP2-c_M" 0 1 0.525490196078431
+ShowClones.scro setCloneColour "aSP3-c_F" 0 1 0.843137254901961
+ShowClones.scro setCloneColour "aSP3-c_M" 0 0.843137254901961 1
+ShowClones.scro setCloneColour "aSP3-e_F" 0 0.525490196078431 1
+ShowClones.scro setCloneColour "aSP3-e_M" 0 0.211764705882353 1
+ShowClones.scro setCloneColour "aSP3-g_F" 0.105882352941176 0 1
+ShowClones.scro setCloneColour "aSP3-g_M" 0.419607843137255 0 1
+ShowClones.scro setCloneColour "aSP3-i_F" 0.737254901960784 0 1
+ShowClones.scro setCloneColour "aSP3-i_M" 1 0 0.949019607843137
+ShowClones.scro setCloneColour "mAL-a_F" 1 0 0.631372549019608
+ShowClones.scro setCloneColour "mAL-a_M" 1 0 0.317647058823529
