bc5877cffc5cc872695ced91b38abbf84c1c0b25
Gregory Jefferis <jefferis@gmail.com>
2009-11-16 20:20:39 +0000

    Give ResampleAndFlipMasks a default gzip option

--- R/Code/ImageProcessingFunctions.R
+++ R/Code/ImageProcessingFunctions.R
@@ -2,7 +2,7 @@
 # distinct from ImageAnalyisFunctions which are more statistical in nature
 # this is really about munging files
 
-ResampleAndFlipMasks<-function(masks,outdir,FlipBridgingReg,flipAxis=c("X","Y","Z"),targetspec){
+ResampleAndFlipMasks<-function(masks,outdir,FlipBridgingReg,flipAxis=c("X","Y","Z"),targetspec,gzip=TRUE){
 	flipAxis=match.arg(flipAxis)
 	if(!file.exists(outdir)) dir.create(outdir)
 	for (infile in masks){
@@ -29,7 +29,7 @@ ResampleAndFlipMasks<-function(masks,outdir,FlipBridgingReg,flipAxis=c("X","Y","
 			filesToIgnoreModTimes=horizontalFlipReg, OverWrite='update', Verbose=T,
 			output=flippedresampledfile,reformatoptions="-v --pad-out 0 --nn",dryrun=FALSE)
 		# and OR ing those results
-		Nrrd2op(c(resampledfile,flippedresampledfile),orfile,'max')
+		Nrrd2op(c(resampledfile,flippedresampledfile),orfile,'max',gzip=gzip)
 	}
 	unlink(horizontalFlipReg)
 }
