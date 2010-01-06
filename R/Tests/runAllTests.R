eb293ae866e48d43ed897f99b78b91f68c9c66d3
Gregory Jefferis <jefferis@gmail.com>
2010-01-06 15:47:41 +0000

    Run TestSuites defined by subfolders of TestDir
    * also print a nice HTML report (in the test folder itself at the moment)

--- R/Tests/runAllTests.R
+++ R/Tests/runAllTests.R
@@ -4,9 +4,10 @@
 # source(file.path(RootDir,"R","Tests","runAllTests.R"))
 
 require(RUnit)
-TestDirs=file.path(RootDir,"R","Tests")
+TestDirs=rownames(subset(file.info(dir(TestDir,full=TRUE)),isdir==TRUE))
 cat("Run all tests:\n")
 MyTestSuites<-lapply(TestDirs,function(x) defineTestSuite(basename(x),x,testFileRegexp="^runit.+.[srR]{1}$") )	
-TestResults=lapply(MyTestSuites,runTestSuite)
+TestResults=runTestSuite(MyTestSuites)
 print(TestResults)
+printHTMLProtocol(TestResults,fileName=file.path(TestDir,"LatestTestResults.html"))
 rm(TestDirs,MyTestSuites)
