bcf5d658898c9d7374f3c27431ec9838577805f2
Gregory Jefferis <jefferis@gmail.com>
2010-01-10 17:00:45 +0000

    Add new TestSuite for ImageProcessing

---
+++ R/Tests/ImageProcessing/runitGeneralImageProcessing.R
@@ -0,0 +1,22 @@
+test.ReadWriteIdentityRegistration<-function(){
+	ireg=structure(c(0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0), 
+		.Dim = c(5L, 3L), .Dimnames = list(c("xlate", "rotate", "scale", "shear", "center"),
+		 c("X", "Y", "Z")))
+	f=WriteIdentityRegistration()
+	x=ReadIGSRegistration(f)
+	checkEqualsNumeric(ireg,do.call(rbind,x$affine_xform))
+	unlink(f)
+}
+
+test.AutoCropNrrd<-function(){
+	tmpdir=tempfile()
+	dir.create(tmpdir)
+	lhmaskfile=file.path(ObjDir,"LHMask.nrrd")
+	AutoCropNrrd(lhmaskfile,outfile=file.path(tmpdir,"LHMask.nrrd"))
+	h=ReadNrrdHeader(file.path(tmpdir,"LHMask.nrrd"))
+	imageSize=c(44,42,40)
+	spaceOrigin=c(4.2,7.0,5.6)
+	checkEqualsNumeric(h$sizes,imageSize,msg="Mismatch with expected image dimensions in pixels")
+	checkEqualsNumeric(h$`space origin`,spaceOrigin,msg="Mismatch with expected image origin (physical coords)")
+	unlink(tmpdir,recursive=TRUE)	
+}
