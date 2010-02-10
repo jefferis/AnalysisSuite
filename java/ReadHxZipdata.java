967421bdd6b40ca3d2568116ea9ac651f5fa0da9
Gregory Jefferis <jefferis@gmail.com>
2010-02-10 23:17:36 +0000

    read compressed hzxip data into memory to avoid apparent bug in inflater
    * seems to be failing silently when read can't keep up?

--- java/ReadHxZipdata.java
+++ java/ReadHxZipdata.java
@@ -2,27 +2,37 @@
 // Amiramesh files using the HxZip encoding
 // (c) Copyright 2010 Gregory Jefferis. All Rights Reserved. 
 
+// Note that this seems to fail silently when decompressing files over AFP
+// Really can't think why
+
+// OK more testing reveals silent failures on local disk as well, 
+// when decompressing whole buffer at once
+
+// Solution appears to be to read compressed data into memory first
+
+
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.BufferedInputStream;
-import java.util.zip.InflaterInputStream;
+import java.util.zip.Inflater;
 
 public class ReadHxZipdata {
 	
 	public static void main(String [ ] args)
 	{
-		if(args.length!=4) {
-			System.err.println("usage: java ReadHxZipdata <infile> <offset> <uncompressedDatalength> <outfile>");
+		if(args.length!=5) {
+			System.err.println("usage: java ReadHxZipdata <infile> <offset> <compressedDatalength> <uncompressedDatalength> <outfile>");
 			return;
 		}
 
 		String file=args[0];
-		String outfile=args[3];
-		int offset=0,dataLength=0;
+		String outfile=args[4];
+		int offset=0,dataLength=0,compressedDataLength=0;
 		try {
 			// the String to int conversion happens here
 			offset = Integer.parseInt(args[1].trim());
-			dataLength = Integer.parseInt(args[2].trim());
+			compressedDataLength = Integer.parseInt(args[2].trim());
+			dataLength = Integer.parseInt(args[3].trim());
 			System.err.println("dataLength is "+dataLength+" bytes");
 	    }
 	    catch (NumberFormatException nfe)
@@ -32,25 +42,25 @@ public class ReadHxZipdata {
 		
 		try{
 			
+			// First read compressed data
 			FileInputStream im = new FileInputStream(file);
-			
-			im.skip(offset);
 			System.err.println("Skipping "+offset+" bytes");
 			BufferedInputStream bis = new BufferedInputStream(im);
-			InflaterInputStream decompressor = new InflaterInputStream(bis);
-			// long bytesread = decompressor.skip(dataLength);
-			byte[] buf = new byte[dataLength];
-			int i=0;
-			for(; i<dataLength && decompressor.available()==1 ;i++){
-				buf[i]=(byte) decompressor.read();
-			}
-			int bytesread = i;
-			// int bytesread = decompressor.read(buf,0,dataLength);
+			bis.skip(offset);
+			byte[] compressedData = new byte[compressedDataLength];
+			bis.read(compressedData);
 			im.close();
-			decompressor.close();
+
+			// Now decompress data
+			Inflater decompressor = new Inflater();
+			byte[] buf = new byte[dataLength];
+			decompressor.setInput(compressedData, 0, compressedDataLength);
+			int bytesread = decompressor.inflate(buf);
+			decompressor.end();
 			
 			System.err.println("Read "+bytesread+" bytes");
 			
+			// Now write output
 			FileOutputStream fos = new FileOutputStream(outfile);
 			fos.write(buf);
 			fos.close();
