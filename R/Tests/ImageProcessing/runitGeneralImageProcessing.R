52a2da5d5cf985e252549378e3666ea05168b1e5
Gregory Jefferis <jefferis@gmail.com>
2010-09-20 19:43:14 +0100

    test.AddOrReplaceNrrdHeaderField: check addition of new field works

--- R/Tests/ImageProcessing/runitGeneralImageProcessing.R
+++ R/Tests/ImageProcessing/runitGeneralImageProcessing.R
@@ -26,8 +26,11 @@ test.AddOrReplaceNrrdHeaderField<-function(){
 	dir.create(tmpdir)
 	lhmaskfile=file.path(ObjDir,"LHMask.nrrd")
 	AddOrReplaceNrrdHeaderField(lhmaskfile,outfile=file.path(tmpdir,"LHMask.nrrd"),"space origin","(0,0,0)")
+	AddOrReplaceNrrdHeaderField(lhmaskfile,outfile=file.path(tmpdir,"LHMask.nrrd"),"fakefield","(1,2,3)",Force=TRUE)
 	h=ReadNrrdHeader(file.path(tmpdir,"LHMask.nrrd"))
 	newSpaceOrigin=c(0,0,0)
+	fakefield=c(1,2,3)
 	checkEqualsNumeric(h$`space origin`,newSpaceOrigin,msg="Mismatch with expected image origin (physical coords)",tol=1e-6)
-	unlink(tmpdir,recursive=TRUE)	
+	checkEqualsNumeric(h$`fakefield`,fakefield,msg="Mismatch with fake field",tol=1e-6)
+	unlink(tmpdir,recursive=TRUE)
 }
