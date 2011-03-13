7f6b81a578bc1d7cc040b79f6ed44bb6dba1ef7b
Gregory Jefferis <jefferis@gmail.com>
2011-03-13 11:26:42 +0000

    Fix test for Longair traces, add test for Neurolucida

--- R/Tests/IO/runitNeuronFunctions.R
+++ R/Tests/IO/runitNeuronFunctions.R
@@ -6,25 +6,32 @@
 
 # runTestFile(file.path(TestDir,"IO","runitNeuronFunctions.R"))
 require(RUnit)
-
 test.read.neuron<-function(){
-	fieldsToCheck=c("NeuronName", "NumPoints", "StartPoint", "BranchPoints", "EndPoints", 
-	"NumSegs", "SegList", "nTrees", "d", "OrientInfo")
-
 	result.new=read.neuron(file.path(TestDir,"IO","Neurites.am"))
 	result=ReadNeuronFromAM3D(file.path(TestDir,"IO","Neurites.am"))
-
-	checkEquals(result[fieldsToCheck],result.new[fieldsToCheck],tol=1e-6)
+	checkTrue(is.neuron(result.new))
+	checkEquals(result,result.new)
 	
 	result.new=read.neuron(file.path(TestDir,"IO","SequentiallyBranchingTrace.traces"))
-	result=ReadNeuronsFromLongairTraces(file.path(TestDir,"IO","SequentiallyBranchingTrace.traces"))
+	result=ReadNeuronsFromLongairTraces(
+		file.path(TestDir,"IO","SequentiallyBranchingTrace.traces"),Merge=TRUE)
+	checkTrue(is.neuron(result.new))
+	checkEquals(result.new,result)
 	
-	checkException(read.neuron(file.path(TestDir,"IO","runitNeuronFunctions.R")))
+	result.new=read.neuron(file.path(TestDir,"IO","LF28R.tasc"))
+	result=ReadNeuronFromAsc(file.path(TestDir,"IO","LF28R.tasc"))
+	checkTrue(is.neuron(result.new))
+	checkEquals(result,result.new)
+	
+	checkException(read.neuron(file.path(TestDir,"IO","runitNeuronFunctions.R")),
+		silent=TRUE)
 }
+# debug(test.read.neuron)
 
 test.all.equal.neuron<-function(){
 	a=ReadNeuronFromAM3D(file.path(TestDir,"IO","Neurites.am"))
-	b=ReadNeuronsFromLongairTraces(file.path(TestDir,"IO","SequentiallyBranchingTrace.traces"))
+	b=ReadNeuronsFromLongairTraces(
+		file.path(TestDir,"IO","SequentiallyBranchingTrace.traces"))
 	checkTrue(all.equal(a, a))
 	checkTrue(!isTRUE(all.equal(a, b)))
 	checkTrue(!isTRUE(all.equal(a, NULL)))
