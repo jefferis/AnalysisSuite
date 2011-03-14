0911515bb1d61236e61c255dcbb5a41c0b4c1823
Gregory Jefferis <jefferis@gmail.com>
2011-03-14 11:47:07 +0000

    Fix check for 3 columns

--- R/Code/ImagesToDots.R
+++ R/Code/ImagesToDots.R
@@ -5,7 +5,7 @@
 DotProperties<-function(points,k=20){
 	npoints=nrow(points)
 	if(npoints<k) stop("Too few points to calculate properties")
-	if(ncol(npoints)!=3) stop("points must be a N x 3 matrix")
+	if(ncol(points)!=3) stop("points must be a N x 3 matrix")
 	
 	alpha=rep(0,npoints)
 	vect=matrix(0,ncol=3,nrow=npoints)
@@ -26,11 +26,7 @@ DotProperties<-function(points,k=20){
 		inertia[1,3]<-inertia[3,1]<-sum(cpt[1,]*cpt[3,])
 		inertia[2,3]<-inertia[3,2]<-sum(cpt[2,]*cpt[3,])
 		
-		# call internal LAPACK routine of eigen directly
-		z<-.Call("La_rs", inertia, only.values=FALSE, PACKAGE = "base")
-		ord <- rev(seq_along(z$values))
-		v1d1=list(values = z$values[ord], vectors = z$vectors[,ord, drop = FALSE])
-
+		v1d1=eigen(inertia,symmetric=TRUE)
 		alpha[i]=(v1d1$values[1]-v1d1$values[2])/sum(v1d1$values)
 		vect[i,]=v1d1$vectors[,1]
 	}
