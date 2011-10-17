7b440816bbf91dd839329d06fac7e0e11963279b
Gregory Jefferis <jefferis@gmail.com>
2011-10-17 05:33:55 +0100

    Add arithmetic functions for neuron coordinates & scale.neuron
    * scale and arithmetic functions somewhat redundant
    * would  be good to add dotprops versions
      (for which it might be worth to making new base class)

--- R/Code/NeuronFunctions5.s
+++ R/Code/NeuronFunctions5.s
@@ -80,6 +80,67 @@ is.neuronlist<-function(nl) {
 		(is.list(nl) && length(nl)>1 && is.neuron(nl[[1]]))
 }
 
+#' Arithmetic for neuron coordinates
+#'
+#' If x is one number or 4-vector, multiply xyz and diameter by that
+#' If x is a 3-vector, multiply xyz only
+#' TODO Figure out how to document arithemtic functions in one go
+#' @param n a neuron
+#' @param x (a numeric vector to multiply neuron coords in neuron)
+#' @return modified neuron
+#' @export
+#' @examples
+#' n1<-MyNeurons[[1]]*2
+#' n2<-MyNeurons[[1]]*c(2,2,2,2)
+#' stopifnot(all.equal(n1,n2))
+#' n3<-MyNeurons[[1]]*c(2,2,4)
+"*.neuron" <- function(n,x) {
+	# TODO look into S3 generics for this functionality
+	
+	nd=n$d[,c("X","Y","Z","W")]
+	stopifnot(is.numeric(x))
+	lx=length(x)
+	if(lx==1) nd[,-4]=nd[,-4]*x
+	else if(lx%in%c(3,4)) nd[,1:lx]=t(t(nd[,1:lx])*x)
+	else stop("expects a numeric vector of length 1, 3 or 4")
+	n$d[,colnames(nd)]=nd
+	n
+}
+
+"+.neuron" <- function(n,x) {
+	if(!is.numeric(x))
+		stop("expects a numeric vector")
+	nd=n$d[,c("X","Y","Z","W")]
+	lx=length(x)
+	if(lx==1) nd[,-4]=nd[,-4]+x
+	else if(lx%in%c(3,4)) nd[,1:lx]=t(t(nd[,1:lx])+x)
+	else stop("expects a numeric vector of length 1, 3 or 4")
+	n$d[,colnames(nd)]=nd
+	n
+}
+
+"-.neuron" <- function(n,x) n+(-x)
+"/.neuron" <- function(n,x) n*(1/x)
+
+#' Divide neuron coords by a factor (and optionally center)
+#'
+#' Note that if scale=TRUE, the neuron will be rescaled to unit sd in each axis
+#' likewise if center=TRUE, the neuron will be centred around the axis means
+#' @param scale 3-vector used to divide x,y,z coords
+#' @param center 3-vector to subtract from x,y,z coords
+#' @return neuron with scaled coordinates
+#' @export
+#' @seealso \code{\link{scale.default}}
+#' @examples
+#' n1.scaledown=scale(MyNeurons[[1]],c(2,2,3))
+#' n1.scaleup=scale(MyNeurons[[1]],1/c(2,2,3))
+scale.neuron<-function(n,scale,center=F){
+	d=xyzmatrix(n)
+	ds=scale(d,scale=scale,center=center)
+	n$d[,colnames(d)]=ds
+	n
+}
+
 all.equal.neuron<-function(target,current,tolerance=1e-6,check.attributes=FALSE,
 	fieldsToCheck=c("NeuronName", "NumPoints", "StartPoint", "BranchPoints",
 		"EndPoints", "NumSegs", "SegList", "d"), fieldsToCheckIfPresent="nTrees",
