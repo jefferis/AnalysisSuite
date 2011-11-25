8c2a80e3621ff5aaa1ca20fc7f3d1a9e68756eed
Gregory Jefferis <jefferis@gmail.com>
2011-11-25 15:45:39 +0000

    Add tests for RunCmdForNewerInput

--- R/Tests/Core/runitOSUtils.R
+++ R/Tests/Core/runitOSUtils.R
@@ -13,7 +13,8 @@ test.abs2rel<-function(x)
 	realans="Sebastian/images"
 	checkEquals(testans,realans)
 		
-	checkException(abs2rel("/some/other/path","/Volumes/JData/JPeople/",Stop=TRUE))
+	checkException(abs2rel("/some/other/path","/Volumes/JData/JPeople/",
+		StopIfNoCommonPath=TRUE))
 	
 	testans=abs2rel("/some/other/path","/Volumes/JData/JPeople/")
 	realans="/some/other/path"
@@ -32,4 +33,36 @@ test.fix.dir<-function(){
 	testans = fix.dir(c("/some/path/","/some/other/path"))
 	realans = c("/some/path/","/some/other/path/")
 	checkEquals(testans,realans)	
+}
+
+test.RunCmdForNewerInput<-function(){
+	# make a test directory
+	tf=replicate(5,tempfile())
+	for (i in 1:2) cat("Hello",i,"!",file=tf[i])
+	Sys.sleep(1.5)
+	for (i in 3:4) cat("Hello",i,"!",file=tf[i])
+	on.exit(unlink(tf[1:4]))
+
+	# one older input file
+	checkEquals(RunCmdForNewerInput(NULL,infiles=tf[1],outfile=tf[3]),FALSE)
+
+	# one newer input file
+	checkTrue(RunCmdForNewerInput(NULL,infiles=tf[4],outfile=tf[1]))
+
+	# multiple older inputfiles
+	checkEquals(FALSE,
+		RunCmdForNewerInput(NULL,infiles=c(tf[1],tf[2]),outfile=tf[3]))
+
+	# one newer and one older input file
+	checkTrue(RunCmdForNewerInput(NULL,infiles=c(tf[4],tf[1]),outfile=tf[2]))
+
+	# single missing input file
+	checkEquals(RunCmdForNewerInput(NULL,infiles=tf[5],outfile=tf[2]),FALSE)
+
+	# one input missing, another present
+	checkEquals(FALSE,
+		RunCmdForNewerInput(NULL,infiles=c(tf[1],tf[5]),outfile=tf[2]))
+
+	# missing output file 
+	checkTrue(RunCmdForNewerInput(NULL,infiles=c(tf[1],tf[4]),outfile=tf[5]))
 }
