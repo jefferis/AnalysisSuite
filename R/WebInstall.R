602928a02814cbfa2ec85d13c80eb90dd6334238
Gregory Jefferis <jefferis@gmail.com>
2012-08-08 21:15:12 +0100

    More information in WebInstall
    * but no logic changes

--- R/WebInstall.R
+++ R/WebInstall.R
@@ -1,12 +1,13 @@
 # This script is designed to be almost invariant and hosted at 
 # an http URL so that it can be sourced in R.
+# (R cannot directly source the https urls that are now required by github)
 
 if(!require(httr)){
   message("Installing httr package to permit https downloads (from github)")
   install.packages("httr")
   if(!require(httr)) {
     browseURL("https://github.com/jefferis/AnalysisSuite/blob/master/README.md")
-    stop("Manual install required.")
+    stop("Manual install required. See section 'R Install', subsection 'Manual Install' of README.md")
   }
 }
 
