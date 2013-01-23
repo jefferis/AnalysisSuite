4b6ceeb8a5150f149cb626e89d49b74f97985577
Gregory Jefferis <jefferis@gmail.com>
2013-01-23 07:55:00 +0000

    Save some time by reading segments brew template once only.

--- R/Code/IO/NeuroML.R
+++ R/Code/IO/NeuroML.R
@@ -5,6 +5,8 @@ dotprops2nml<-function(x,f,id,neurite_diam=1,soma_diam=4,notes=NULL,...){
 	require(brew)
 	neuroml_tpl=file.path(CodeDir,'IO','NeuroML.brew')
 	seg_tpl=file.path(dirname(neuroml_tpl),'NeuroML_segment.brew')
+	# save time by reading this to memory once
+	seg_tpl_txt=paste(readLines(seg_tpl),collapse='\n')
 
 	if(is(f,'connection')) con<-f
 	else con=file(f,open='wt')
@@ -21,12 +23,12 @@ dotprops2nml<-function(x,f,id,neurite_diam=1,soma_diam=4,notes=NULL,...){
 				segment_id=i
 				p1=c(x$points[i,]-x$vect[i,]/2,neurite_diam)
 				p2=c(x$points[i,]+x$vect[i,]/2,neurite_diam)
-				brew(seg_tpl,tc)
+				brew(text=seg_tpl_txt,output=tc)
 			}
 		} else if(grepl('soma',btpl)){
 			segment_id=0
 			p1=p2=c(unlist(x$soma),soma_diam)
-			brew(seg_tpl,tc)
+			brew(text=seg_tpl_txt,output=tc)
 		}
 		
 		paste(segments,collapse='\n')
