c7932c63b01284953830ceca7f1f52ba2d291445
GJAC <jefferis@mrc-lmb.cam.ac.uk>
2013-05-31 14:23:37 +0100

    Corrected minor mistake in function

--- R/Code/Image/ImageProcessingFunctions.R
+++ R/Code/Image/ImageProcessingFunctions.R
@@ -50,24 +50,24 @@ ResampleImages<-function(images,outdir,targetspec,registrations,suffix="-resampl
 #' @seealso \code{\link{ReformatImage},\link{FlipAndORMasks}}
 FlipImagesWithBridgingReg<-function(images,outdir,FlipBridgingReg,flipAxis=c("X","Y","Z"),
 	gzip=TRUE,...){
-		flipAxis=match.arg(flipAxis)
-		if(!file.exists(outdir)) dir.create(outdir)
-		outfiles=file.path(outdir,
-			sub(".nrrd$","-flip.nrrd",basename(infile)))
-		for (i in seq(masks)){
-			infile=masks[i]
-			outfile=outfiles[i]
-			# make flipping registration
-			if(!exists("horizontalFlipReg") || !file.exists(horizontalFlipReg))
-				horizontalFlipReg=WriteFlipRegistration(infile,axis=flipAxis)
-			# and flip all masks, applying the horiz bridging registration
-			ReformatImage(infile,target=infile,
-				registrations=c(FlipBridgingReg,horizontalFlipReg),
-				filesToIgnoreModTimes=horizontalFlipReg, OverWrite='update', Verbose=T,
-				output=outfile,...)
-		}
-		unlink(horizontalFlipReg)
-	outfiles
+	flipAxis=match.arg(flipAxis)
+	if(!file.exists(outdir)) dir.create(outdir)
+	outfiles=file.path(outdir,
+		sub(".nrrd$","-flip.nrrd",basename(images)))
+	for (i in seq(images)){
+		infile=images[i]
+		outfile=outfiles[i]
+		# make flipping registration
+		if(!exists("horizontalFlipReg") || !file.exists(horizontalFlipReg))
+			horizontalFlipReg=WriteFlipRegistration(infile,axis=flipAxis)
+		# and flip all images, applying the horiz bridging registration
+		ReformatImage(infile,target=infile,
+			registrations=c(FlipBridgingReg,horizontalFlipReg),
+			filesToIgnoreModTimes=horizontalFlipReg, OverWrite='update', Verbose=T,
+			output=outfile,...)
+	}
+	unlink(horizontalFlipReg)
+	invisible(outfiles)
 }
 
 FlipAndORMasks<-function(masks,outdir,FlipBridgingReg,flipAxis=c("X","Y","Z"),gzip=TRUE){
