493406834dfb089bd920d75cfe7df10aefed885f
Ashley Manton <ajd.manton@googlemail.com>
2013-11-19 03:19:42 +0000

    Fix WithAllPoints and WithLine in plot3d.neuron

--- R/Code/Core/plotneuron3d.R
+++ R/Code/Core/plotneuron3d.R
@@ -54,10 +54,17 @@ plotneuron3d.simple<-function(ANeuron, WithLine=T,NeuronNames=FALSE,
 	
 	if(WithNodes){
 		Colour=col
-		if(!WithLine) NodeCols=rep(Colour,length(NodeCols))
-		rglreturnlist[["points"]]=points3d(ANeuron$d[NodesOnly,c("X","Y","Z")],color=NodeCols,size=3)
-		if(WithText) # text labels for nodes
-		rglreturnlist[["texts"]]=texts3d(ANeuron$d[NodesOnly,c("X","Y","Z")],text=NodesOnly,color=NodeCols,adj=c(0,0.5))
+		if(WithAllPoints){
+			if(!WithLine) NodeCols=rep(Colour,nrow(ANeuron$d))
+			rglreturnlist[["points"]]=points3d(ANeuron$d[,c("X","Y","Z")],color=NodeCols,size=3)
+			if(WithText) # text labels for nodes
+				rglreturnlist[["texts"]]=texts3d(ANeuron$d[,c("X","Y","Z")],text=seq(nrow(ANeuron$d)),color=NodeCols,adj=c(0,0.5))
+		} else {
+			if(!WithLine) NodeCols=rep(Colour,length(NodeCols))
+			rglreturnlist[["points"]]=points3d(ANeuron$d[NodesOnly,c("X","Y","Z")],color=NodeCols,size=3)
+			if(WithText) # text labels for nodes
+			rglreturnlist[["texts"]]=texts3d(ANeuron$d[NodesOnly,c("X","Y","Z")],text=NodesOnly,color=NodeCols,adj=c(0,0.5))
+		}
 	}
 
 	if(PlotSubTrees && !is.null(ANeuron$SubTrees)) ANeuron$SegList=unlist(ANeuron$SubTrees,recursive=FALSE)
@@ -65,12 +72,15 @@ plotneuron3d.simple<-function(ANeuron, WithLine=T,NeuronNames=FALSE,
 	d=data.matrix(ANeuron$d[,c("X","Y","Z")])
 	# xyzl=sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)})
 	# NAs are used to break line segments
-	xyzl<-do.call(rbind,sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)},simplify=FALSE))
-	rglreturnlist[["lines"]]=lines3d(xyzl,col=col,...)
-	if(HighlightLongestSegment){
-		x=GetLongestSegment(ANeuron)
-		rglreturnlist[["spheres"]]=spheres3d(x,col=col,...)
+	if(WithLine){
+		xyzl<-do.call(rbind,sapply(ANeuron$SegList,function(s) {rbind(d[s,],NA)},simplify=FALSE))
+		rglreturnlist[["lines"]]=lines3d(xyzl,col=col,...)
+		if(HighlightLongestSegment){
+			x=GetLongestSegment(ANeuron)
+			rglreturnlist[["spheres"]]=spheres3d(x,col=col,...)
+		}
 	}
+
 	if(is.logical(NeuronNames) && NeuronNames) NeuronNames=ANeuron$NeuronName
 	if(!is.logical(NeuronNames)){
 		StartPoint=ifelse(is.null(ANeuron$StartPoint),1,ANeuron$StartPoint)
