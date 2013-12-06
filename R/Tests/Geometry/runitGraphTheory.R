06e9e2a1112ec2fbec247bf0c5e669117bc3e3b7
Gregory Jefferis <jefferis@gmail.com>
2013-12-06 19:54:19 +0000

    comprehensive tests for CoreNeuronFromGraph

--- R/Tests/Geometry/runitGraphTheory.R
+++ R/Tests/Geometry/runitGraphTheory.R
@@ -91,6 +91,46 @@ test.CoreNeuronFromGraph<-function(){
   g<-as.igraph(testn)
   cn=CoreNeuronFromGraph(g,1)
   checkEquals(testn$SegList,cn$SegList)
+  
+  # vertex labels with gaps
+  g=neurongraph(c(2,4,4,3,3,6,6,9,6,7),vertexlabels=c(2:4,6,7,9))
+  sl=list(c(1,3,2,4),c(4,5),c(4,6))
+  checkEquals(graph2seglist(g,origin=1),sl)
+  checkEquals(CoreNeuronFromGraph(g,origin=2)$SegList,sl)
+  # same but no origin specified (should give same result)
+  checkEquals(CoreNeuronFromGraph(g)$SegList,sl)
+  
+  # same but different origin
+  sl2=list(c(3,1),c(3,2,4),c(4,5),c(4,6))
+  checkEquals(CoreNeuronFromGraph(g,origin=4)$SegList,sl2)
+  
+  # same connectivity but one extra (floating) point at end
+  g=neurongraph(c(2,4,4,3,3,6,6,9,6,7),vertexlabels=c(2:4,6,7,9,10))
+  n=CoreNeuronFromGraph(g,origin=4)
+  checkEquals(n$SegList,sl2)
+  checkEquals(n$nTrees,2)
+  checkEquals(n$SubTrees,list(sl2,list(7)))
+  
+  # same connectivity but with extra (floating) points at start and end
+  g=neurongraph(c(2,4,4,3,3,6,6,9,6,7),vertexlabels=c(1:4,6,7,9,10))
+  # this will shift all vertex ids by 1
+  sl3=lapply(sl2,'+',1)
+  n=CoreNeuronFromGraph(g,origin=4)
+  checkEquals(n$SegList,sl3)
+  checkEquals(n$nTrees,3)
+  checkEquals(n$SubTrees,list(sl3,list(1),list(8)))
+  
+  # 3 separate subgraphs of length 3,4,5
+  g=neurongraph(c(0,1,1,2, 3,4,4,5,5,6, 7,8,8,9,9,10,10,11),vertexlabels=0:11)
+  n=CoreNeuronFromGraph(g,origin=0)
+  checkEquals(n$SegList,list(c(1,2,3)))
+  n2=CoreNeuronFromGraph(g,origin=3)
+  checkEquals(n2$SegList,list(c(4,5,6,7)))
+  n3=CoreNeuronFromGraph(g,origin=7)
+  checkEquals(n3$SegList,list(c(8,9,10,11,12)))
+  # check that it picks largest subgraph when no origin specified
+  n4=CoreNeuronFromGraph(g)
+  checkEquals(n4,n3)
 }
 
 test.nodes<-function(){
