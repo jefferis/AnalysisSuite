e0f11a113a80f09489e683fa9b663748e4f34201
Gregory Jefferis <jefferis@gmail.com>
2014-01-08 21:30:21 +0000

    deprecate DotProperties

--- R/Code/NeuroBlast/ImagesToDots.R
+++ R/Code/NeuroBlast/ImagesToDots.R
@@ -55,49 +55,11 @@ if(R.version$major<3){
   .internal_lars<-function(x) .Internal(La_rs(x, only.values=FALSE))
 }
 
+#' Deprecated method to calculate dotprops
+#' see nat::dotprops
 DotProperties<-function(points,k=20,UseLabels=TRUE,na.rm=FALSE){
-	# store labels from SWC format data if this is a neuron
-	Labels=if(UseLabels && is.neuron(points)) points$d$Label else NULL
-	points=xyzmatrix(points)
-	if(na.rm){
-		narows=rowSums(is.na(points))>0
-		if(any(narows)) points=points[!narows,]
-	}
-	npoints=nrow(points)
-	if(npoints<k) stop("Too few points to calculate properties")
-	if(ncol(points)!=3) stop("points must be a N x 3 matrix")
-	
-	alpha=rep(0,npoints)
-	vect=matrix(0,ncol=3,nrow=npoints)
-
-	nns=nn2(points,points,k=k)
-	# transpose points to 3xN because 
-	# R arithemtic of matric / vector operates column-wise
-	pointst=t(points)
-	for(i in 1:npoints){
-		indNN=nns$nn.idx[i,]
-		
-		pt=pointst[,indNN]
-		cpt=pt-rowMeans(pt)
-		
-		inertia=matrix(0,ncol=3,nrow=3)
-		diag(inertia)=rowSums(cpt^2)
-		inertia[1,2]<-inertia[2,1]<-sum(cpt[1,]*cpt[2,])
-		inertia[1,3]<-inertia[3,1]<-sum(cpt[1,]*cpt[3,])
-		inertia[2,3]<-inertia[3,2]<-sum(cpt[2,]*cpt[3,])
-		
-		# call internal LAPACK routine of eigen directly
-		z<-.internal_lars(inertia)
-		ord <- rev(seq_along(z$values))
-		v1d1=list(values = z$values[ord], vectors = z$vectors[,ord, drop = FALSE])
-
-		alpha[i]=(v1d1$values[1]-v1d1$values[2])/sum(v1d1$values)
-		vect[i,]=v1d1$vectors[,1]
-	}
-	rlist=list(points=points,alpha=alpha,vect=vect)
-	rlist$labels=Labels
-	attr(rlist,'k')=k
-	return(as.dotprops(rlist))
+	.Deprecated('dotprops','nat')
+	nat::dotprops(x=points,k=k,Labels=UseLabels,na.rm=TRUE)
 }
 
 ind2coord<-function(inds, ...) UseMethod("ind2coord")
