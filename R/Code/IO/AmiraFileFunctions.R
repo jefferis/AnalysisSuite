b762add033a2949b3bc5865d1b4f68c7f751919a
Ashley Manton <ajd.manton@googlemail.com>
2014-01-10 18:50:26 +0000

    Improve regex for colour extraction to deal with trailing comma

--- R/Code/IO/AmiraFileFunctions.R
+++ R/Code/IO/AmiraFileFunctions.R
@@ -1256,8 +1256,8 @@ ParseAMSurfToContourList<-function(filename,RegionNames="ALL",RegionChoice="Inne
 		# Extract colour information
 		colorLine <- grep("Color", headerLines[headerSecStart:headerSecEnd], value=T)
 		if(length(colorLine) > 0) {
-			rgbValues <- strsplit(regmatches(colorLine, regexpr("[0-9].*", colorLine, perl=T)), " ")[[1]]
-			color <- rgb(rgbValues[1], rgbValues[2], rgbValues[3])
+			rgbValues <- strsplit(regmatches(colorLine, gregexpr("[1]|[0-9]\\.[0-9]+", colorLine, perl=T))[[1]], " ")
+			color <- rgb(rgbValues[[1]], rgbValues[[2]], rgbValues[[3]])
 		} else {
 			color <- FallbackRegionCol
 		} 
