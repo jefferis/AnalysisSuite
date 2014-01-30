1fbadb65493c933f25e719b67a64723e318658c4
Gregory Jefferis <jefferis@gmail.com>
2014-01-30 21:35:04 +0000

    Deprecate Read/WriteAmiraLandmarks in favour of nat

--- R/Code/IO/AmiraFileFunctions.R
+++ R/Code/IO/AmiraFileFunctions.R
@@ -1091,45 +1091,14 @@ Read3DDensityFromAmiraLattice<-function(filename,Verbose=FALSE){
 }
 
 ReadAmiraLandmarks<-function(filename,Verbose=FALSE,CoordinatesOnly=TRUE){
-	r=ReadAmiramesh(filename,AttachFullHeader=TRUE,Verbose=Verbose,Simplify=FALSE)
-	headerLines=attr(r,"header")
-	dataDef=attr(r,"dataDef")
-	
-	# get the number of landmark sets
-	NumSetLine=grep("NumSets\\s+[0-9]{1,}",headerLines,value=TRUE)
-	if(length(NumSetLine)==0) stop(paste("Unable to establish number of amira landmarks sets in",filename))
-	nSets=as.numeric(sub(".*NumSets\\s+([0-9]{1,}).*","\\1",NumSetLine))
-	
-	# get the number of data sections
-	nDataSections=nrow(dataDef)
-	nSectionsPerSet=nDataSections/nSets
-	if(round(nSectionsPerSet)!=nSectionsPerSet) stop(paste("Unable to parse amira landmarks sets",filename,":too many data sections!"))
-	
-	if(CoordinatesOnly){
-		chosenSections=seq(from=1,by=nSectionsPerSet,length=nSets)
-		r=r[chosenSections]
-	}
-	if(length(r)==1) r[[1]]
-	else r
+  .Deprecated("read.amiralandmarks","nat")
+  nat::read.amiralandmarks(filename, CoordinatesOnly=CoordinatesOnly, 
+    Verbose=Verbose)
 }
 
 WriteAmiraLandmarks<-function(filename,d){
-	if(is.list(d) && !is.data.frame(d)) l=d
-	else l=list(d)
-	nSets=length(l)
-	nummarkers=sapply(l,nrow)
-	if(length(unique(nummarkers))!=1) stop("Must have just an equal number of markers in paired landmark sets")
-	nummarkers=nummarkers[1]
-	cat("# AmiraMesh 3D ASCII 2.0\n\ndefine Markers",nummarkers,"\n\nParameters {\n\tContentType \"LandmarkSet\",\n\tNumSets",nSets,"\n}\n",file=filename)
-	for(i in 1:nSets){
-		coordsuffix=ifelse(i==1,"",i)
-		cat("Markers { float[3] Coordinates",coordsuffix," } @",i,sep="","\n",file=filename,append=T)
-	}
-	for(i in 1:nSets){
-		cat("@",i,sep="","\n",file=filename,append=T)
-		write.table(l[[i]],col.names=F,row.names=F,file=filename,append=TRUE)
-		cat("\n",file=filename,append=T)
-	}
+  .Deprecated("write.amiralandmarks","nat")
+  nat::write.amiralandmarks(file=filename, x=d)
 }
 
 WriteAmiraColormap<-function(filename,rgba,A=1,minmax=c(0,255)){
